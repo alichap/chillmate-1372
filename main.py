0dcce8f697909b10acea61929ff711ed5119e28a
Antoine Bousquet antoinebousquet8@gmail.com <antoinebousquet8@gmail.com>
2024-02-27 18:04:37 +0100

    main and requirements for a hello world test

---
+++ main.py
@@ -0,0 +1,19 @@
+# Import required libraries
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os, os.path
+import PIL
+from PIL import Image
+import tensorflow as tf
+from PIL import Image
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+import cv2
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras import layers, models
+from tensorflow.keras import optimizers
+
+print('Hello')
