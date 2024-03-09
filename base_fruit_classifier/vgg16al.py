75a882a3edae1a730f4ccfe98fa04188855cd907
Aline Chapuis <chapuis.aline25@gmail.com>
2024-03-09 15:57:59 +0100

    accept merging from main

--- base_fruit_classifier/vgg16al.py
+++ base_fruit_classifier/vgg16al.py
@@ -1,41 +1,45 @@
-from tensorflow.keras.applications import ResNet50
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.models import Model
-from tensorflow.keras.callbacks import EarlyStopping
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-
-import tensorflow as tf
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-
 import numpy as np
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+from tensorflow.keras.applications import VGG16
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-import os
-from tensorflow.keras.layers import Dropout
-from base_fruit_classifier.main import *
-from base_fruit_classifier.registry import *
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
+import datetime
 
-def train_vgg16al(dataset_path, num_classes):
-    # Load the pre-trained ResNet50 model without the top layer
-    base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(100, 100, 3))
+def train_vgg16al(dataset_path, num_classes, epochs):
+    # Define the input shape
+    img_height, img_width = 348, 348
+
+    # Load the pre-trained VGG16 model without the top layer
+    base_model = VGG16(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
+
+    # Rescaling layer
+    rescale_layer = layers.Rescaling(1./255)
+
+    # Data augmentation layer
+    data_augmentation = tf.keras.Sequential([
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1),
+    ])
 
     # Add new layers on top of the model
-    x = base_model.output
+    inputs = tf.keras.Input(shape=(img_height, img_width, 3))
+    x = data_augmentation(inputs)
+    x = rescale_layer(x)
+    x = base_model(x, training=False)  # Make sure the base_model runs in inference mode here
     x = GlobalAveragePooling2D()(x)
-    x = Dense(1000, activation='relu')(x)  # Use a single dense layer with 1000 neurons
-    predictions = Dense(num_classes, activation='softmax')(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    outputs = Dense(num_classes, activation='softmax')(x)
 
     # This is the model we will train
-    model = Model(inputs=base_model.input, outputs=predictions)
+    model = Model(inputs, outputs)
 
-    # Freeze all convolutional ResNet50 layers
+    # Freeze all layers in the base VGG16 model
     for layer in base_model.layers:
         layer.trainable = False
 
@@ -44,6 +48,11 @@ def train_vgg16al(dataset_path, num_classes):
 
     # Early stopping callback
     early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, mode='min', restore_best_weights=True)
+
+# TensorBoard callback
+    logdir = "logs/adam/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=logdir, histogram_freq=1)
+
 
     # Configure the dataset for performance
     AUTOTUNE = tf.data.experimental.AUTOTUNE
@@ -54,7 +63,7 @@ def train_vgg16al(dataset_path, num_classes):
         validation_split=0.2,
         subset="training",
         seed=123,
-        image_size=(100, 100),
+        image_size=(img_height, img_width),
         batch_size=32,
         label_mode='categorical'
     ).prefetch(buffer_size=AUTOTUNE)
@@ -64,7 +73,7 @@ def train_vgg16al(dataset_path, num_classes):
         validation_split=0.2,
         subset="validation",
         seed=123,
-        image_size=(100, 100),
+        image_size=(img_height, img_width),
         batch_size=32,
         label_mode='categorical'
     ).prefetch(buffer_size=AUTOTUNE)
@@ -72,9 +81,9 @@ def train_vgg16al(dataset_path, num_classes):
     # Train the model with early stopping
     model.fit(
         train_dataset,
-        epochs=2, # Adjust epochs according to your needs
+        epochs=epochs,
         validation_data=validation_dataset,
-        callbacks=[early_stopping]
+        callbacks=[early_stopping, tensorboard_callback]
     )
 
     return model
@@ -88,4 +97,4 @@ if __name__ == '__main__':
     dataset_bucket_name = "chillmate_tiny_dataset"
 
     num_classes = len(get_dataset_classes(dataset_bucket_name))
-    train_resnet50(dataset_path, num_classes)
+    train(dataset_path, num_classes)
